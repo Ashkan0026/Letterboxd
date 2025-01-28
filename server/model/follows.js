8b2d3091b6a18c20307b5ab96b1605d702410eb6
Ashkan0026 <ashkanian34@gmail.com>
2025-01-28 11:45:09 +0330

    Follows model added

---
+++ server/model/follows.js
@@ -0,0 +1,38 @@
+class Follows {
+    constructor(id, following_user_id, follower_user_id, createdAt) {
+        this._id = id;
+        this._following_user_id = following_user_id;
+        this._follower_user_id = follower_user_id; 
+        this._createdAt = createdAt;
+    }
+
+    // Getter for ID
+    get id() {
+        return this._id;
+    }
+
+    // Getter for following user ID
+    get following_user_id() {
+        return this._following_user_id;
+    }
+
+    // Setter for following user ID
+    set following_user_id(value) {
+        this._following_user_id = value;
+    }
+
+    // Getter for follower user ID
+    get follower_user_id() {
+        return this._follower_user_id;
+    }
+
+    // Setter for follower user ID
+    set follower_user_id(value) {
+        this._follower_user_id = value;
+    }
+
+    // Getter for createdAt
+    get createdAt() {
+        return this._createdAt;
+    }
+}
