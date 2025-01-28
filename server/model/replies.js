d30c637692805d3589c79755646fb6010223fd8c
Ashkan0026 <ashkanian34@gmail.com>
2025-01-28 11:50:08 +0330

    replies model added

---
+++ server/model/replies.js
@@ -0,0 +1,54 @@
+class Reply {
+    constructor(id, score, desc, user_id, movie_id) {
+        this._id = id;
+        this._score = score;
+        this._desc = desc;
+        this._user_id = user_id;
+        this._movie_id = movie_id;
+    }
+  
+    // Getter for ID
+    get id() {
+      return this._id;
+    }
+  
+    // Getter for score
+    get score() {
+      return this._score;
+    }
+  
+    // Setter for score
+    set score(value) {
+      this._score = value;
+    }
+  
+    // Getter for description
+    get desc() {
+      return this._desc;
+    }
+  
+    // Setter for description
+    set desc(value) {
+      this._desc = value;
+    }
+  
+    // Getter for user ID
+    get user_id() {
+      return this._user_id;
+    }
+  
+    // Setter for user ID
+    set user_id(value) {
+      this._user_id = value;
+    }
+  
+    // Getter for movie ID
+    get movie_id() {
+      return this._movie_id;
+    }
+  
+    // Setter for movie ID
+    set movie_id(value) {
+      this._movie_id = value;
+    }
+}
